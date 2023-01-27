640d4a169aeb418ac56d409fadd92e3879d985c7
rezzankilic <kilic.rez@gmail.com>
2023-01-27 12:52:41 -0500

    mongoose

--- models/product.js
+++ models/product.js
@@ -1,9 +1,10 @@
-const { default: mongoose } = require('mongoose');
-const mogoose = require('mongoose');
+
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: {type: String, required: true},
     price: {type: Number, required: true}
 });
 
-mongoose.model('Product', productSchema)
+
+module.exports = mongoose.model('Product', productSchema)
---
+++ mongoose.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://rezzan:1223456@cluster0.apsjcns.mongodb.net/products_test?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Connected to datbse')
+}).catch(()=>{
+    console.log('Connection failedd')
+});
+
+const createProduct = async(req, res, next) => {
+    const createdProduct = new Product({
+        name: req.body.name,
+        price: req.body.price
+    });
+
+    const result = await createdProduct.save();
+
+    res.json(result);
+}
+
+const getProducts = async(req, res, next) => {
+    const products = await Product.find().exec();
+    res.json(products)
+}
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
